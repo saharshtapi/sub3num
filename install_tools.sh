735c79c7e49b368631017ad95e75a330188e4c74
saharshtapi <51168156+saharshtapi@users.noreply.github.com>
2021-05-04 21:06:33 +0545

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -37,7 +37,7 @@ cp assetfinder /usr/local/bin >/dev/null 2>&1
 cd
 
 echo "[+]Installing subfinder"
-go get -v -u github.com/projectdiscovery/subfinder/v2/cmd/subfinder >/dev/null 2>&1
+go get -v -u github.com/projectdiscovery/subfinder/v2/cmd/subfinder 
 cd $HOME/go/src/github.com/projectdiscovery/subfinder/v2/cmd/subfinder/>/dev/null 2>&1
 go build >/dev/null 2>&1
 cp subfinder /usr/local/bin >/dev/null 2>&1
@@ -55,7 +55,7 @@ cd
 
 echo "[+]Installing Amass (Might take some time).."
 #go get -v -u github.com/OWASP/Amass/cmd/amass 
-go get -v github.com/OWASP/Amass/v3/... >/dev/null 2>&1
+go get -v github.com/OWASP/Amass/v3/... 
 cd $HOME/go/src/github.com/OWASP/Amass/cmd/amass>/dev/null 2>&1
 go build >/dev/null 2>&1
 cp amass /usr/local/bin >/dev/null 2>&1
