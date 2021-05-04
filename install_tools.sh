e0579c575c60029f4087f0bec90187623d576d98
saharshtapi <51168156+saharshtapi@users.noreply.github.com>
2021-05-04 20:45:22 +0545

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -9,7 +9,7 @@ cd tools
 git clone https://github.com/aboul3la/Sublist3r.git >/dev/null 2>&1
 #echo "[+]Getting update.."
 #sudo apt update >/dev/null 2>&1
-sudo apt install python3 python3-pip >/dev/null 2>&1
+sudo apt install python3 python3-pip -y >/dev/null 2>&1
 echo "[+]Installing requirements"
 pip3 install -r Sublist3r/requirements.txt >/dev/null 2>&1
 ##################
